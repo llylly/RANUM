a447cf36e3cf77ec55907c5957499ca242bfeab3
Linyi Li <limyik.li96@gmail.com>
2022-05-09 17:32:59 -0500

    A beta version
    Artifact submission version
    Did some minor changes irrelevant to existing tool functions
    Need to globally change the tool name

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ proj_root = Path.cwd()
 timeout_for_grist_execution = '10m'
 
 # the following should be removed during submission
-SERVER_PYTHON_PATH = '/home/linyi2/anaconda3/bin/python'
+SERVER_PYTHON_PATH = '********'
---
+++ evaluate/result_copier.py
@@ -0,0 +1,30 @@
+"""
+This script copies files other than .pt from results/ folder
+Since .pt files are tooo large and does not contain useful result statistics
+"""
+import argparse
+import shutil
+import os
+
+cnt = 0
+
+def copy_worker(source, dest):
+    global cnt
+    for file in os.listdir(source):
+        if os.path.isdir(os.path.join(source, file)):
+            if not os.path.exists(os.path.join(dest, file)):
+                os.makedirs(os.path.join(dest, file))
+            copy_worker(os.path.join(source, file), os.path.join(dest, file))
+        else:
+            if not file.endswith('.pt'):
+                cnt += 1
+                print(f'[{cnt}] copy {os.path.join(source, file)} to {os.path.join(dest, file)}')
+                shutil.copy(os.path.join(source, file), os.path.join(dest, file))
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('source', type=str)
+parser.add_argument('dest', type=str)
+if __name__ == '__main__':
+    args = parser.parse_args()
+    copy_worker(args.source, args.dest)
